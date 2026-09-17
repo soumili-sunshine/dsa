8ed9d4bbdc5869ca2f3d306e1d7aeb5c48f01b2b
soumili-sunshine <desoumili0@gmail.com>
2026-09-18 02:38:41 +0530

    Add structure to store and display student data

---
+++ DAY1/Structure_data.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+struct Student {
+    int roll_no;
+    char name[50];
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    printf("Enter Roll Number: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+
+    printf("Enter Marks: ");
+    scanf("%f", &s.marks);
+
+    printf("\n--- Student Details ---\n");
+    printf("Roll Number: %d\n", s.roll_no);
+    printf("Name: %s\n", s.name);
+    printf("Marks: %.2f\n", s.marks);
+
+    return 0;
+}
